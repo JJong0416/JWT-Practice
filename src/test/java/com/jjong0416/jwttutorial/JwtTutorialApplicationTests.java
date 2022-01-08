72d6a37b7b02c6c56ea2e208d936a0abc5e0a092
82108 <whdcks5681@naver.com>
2022-01-08 22:49:27 +0900

    init JWT-Tutorial

---
+++ src/test/java/com/jjong0416/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjong0416.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
